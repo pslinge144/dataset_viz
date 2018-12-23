e3feb2a703b5e824533f5002fe98a3b089ba24dc
phil <pslinge144@gmail.com>
2018-12-23 16:04:23 -0500

    Module confusion...

--- server/create_database.py
+++ server/create_database.py
@@ -3,11 +3,11 @@ from database import add_filename
 
 
 def create_database():
-    database = sqlite.connect("data.db")
-    cursor = database.cursor()
+    db = sqlite.connect("data.db")
+    cursor = db.cursor()
 
     create_cubes_sql = "CREATE TABLE cubes (filename TEXT)"
     cursor.execute(create_cubes_sql)
 
-    database.commit()
-    database.close()
+    db.commit()
+    db.close()
--- tasks/prepare_db.py
+++ tasks/prepare_db.py
@@ -1,1 +1,13 @@
-from server import create_database
+import pathlib
+import json
+import sys
+sys.path.insert(0, '.')
+from server.create_database import create_database
+
+
+CONFIG_FILE = pathlib.Path(__file__).parents[1].resolve() / 'config.json'
+
+
+config = json.load(CONFIG_FILE)
+
+print(config)
