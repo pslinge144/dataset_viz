fdcbe105c213c5edecbab29654749af60a94de0d
phil <pslinge144@gmail.com>
2018-12-26 09:47:19 -0500

    Changed name of directory to reduce confusion.

--- tasks/prepare_db.py
+++ tasks/prepare_db.py
@@ -2,7 +2,7 @@ import pathlib
 import json
 import sys
 sys.path.insert(0, '.')
-from server.create_database import create_database
+from server.server.create_database import create_database
 
 
 CONFIG_FILE = pathlib.Path(__file__).parents[1].resolve() / 'config.json'
